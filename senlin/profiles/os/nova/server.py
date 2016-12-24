315bb09898e7e2d7ca513d48c5b07d4e17a6f8e4
pangxudong <pangxudong0312@gmail.com>
2016-12-24 10:28:38 +0800

    no message

--- senlin/profiles/os/nova/server.py
+++ senlin/profiles/os/nova/server.py
@@ -741,6 +741,25 @@ class ServerProfile(base.Profile):
 
         return True
 
+    def do_cold_migration(self, obj):
+        if not obj.physical_id:
+            return False
+
+        self.server_id = obj.physical_id
+
+        try:
+            workflow_name = "cluster-coldmigration"
+            if (str(self.mistral().workflow_find(workflow_name)) == None):
+                definition = str(open("/opt/stack/senlin/senlin/engine/actions/cluster_migration/cold_migration/cluster-coldmigration.yaml",'r').read())
+                self.mistral().workflow_create(definition,scope="private")
+            input = '{"cluster_id" : "%s", "node_id" : "%s", "flavor_id": "1"}'  % (obj.cluster_id, obj.physical_id)# flavor: target_flavor of resize-migrate
+            self.mistral().execution_create(workflow_name, input)
+        except Exception,e:
+            LOG.error(str(e))
+            return False
+
+        return True
+
     def mistral(self):
             if self._mistralclient is not None:
                 return self._mistralclient
@@ -752,17 +771,7 @@ class ServerProfile(base.Profile):
 
         if 'operation' in options:
             if options['operation'] == "COLD_MIGRATION":
-                try:
-                    workflow_name = "cluster-coldmigration"
-                    if (str(self.mistral().workflow_find(workflow_name)) == None):
-                        definition = str(open("/opt/stack/senlin/senlin/engine/actions/cluster_migration/cold_migration/cluster-coldmigration.yaml",'r').read())
-                        self.mistral().workflow_create(definition,scope="private")
-                    input = '{"cluster_id" : "%s", "node_id" : "%s", "flavor_id": "1"}'  % (obj.cluster_id, obj.physical_id)# flavor: target_flavor of resize-migrate
-                    resp = self.mistral().execution_create(workflow_name, input)
-                except Exception,e:
-                    LOG.error(str(e))
-                    return False
-                return True
+                return self.do_cold_migration(obj)
 
             elif options['operation'] == 'REBUILD':
                 return self.do_rebuild(obj)
