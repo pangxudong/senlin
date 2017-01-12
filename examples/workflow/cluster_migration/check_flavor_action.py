839e98f1c1f3b129b5de46380daea1ea23ad08db
pangxudong <pangxudong0312@gmail.com>
2017-01-12 18:11:41 +0800

    add check hypervisor flavor action

--- examples/workflow/cluster_migration/check_flavor_action.py
+++ examples/workflow/cluster_migration/check_flavor_action.py
@@ -1,19 +1,19 @@
 import sys
 from mistral.actions.openstack.actions import NovaAction
 
+# check hypervisor free resource to allocate for a instance of the flavor type
+class CheckFlavorAction(NovaAction):
 
-class CheckFlavorNovaAction(NovaAction):
-
-    def __init__(self, migrate, tenant_id, flavor_id):
+    def __init__(self, migrate, hostname, flavor_id):
         self._flavor_id = flavor_id
-        self._tenant_id= tenant_id
-        self._migrate= migrate
+        self._hostname = hostname
+        self._migrate = migrate
 
     def run(self):
         client = self._get_client()
 
         if self._migrate:
             flavor_dict = client.flavors.find(id=self._flavor_id).to_dict()
-            limits_dict = client.limits.get().to_dict()
+            limits_dict = client.hypervisors.find(hypervisor_hostname=self._hostname).to_dict()
             # if (server.flavor['id'] != str(self._flavor_id)):
             #     sys.exit("flavor not correct!")
